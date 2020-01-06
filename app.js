65c5138a57c362a8f1ab19eca2f23c2695cd3a3d
masdulloh <ma089983@gmail.com>
2020-01-06 23:52:39 +0700

    localhost port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const rajaRouter = require("./api/rajaongkir/rajaongkir.router");
 const port = process.env.PORT || 4000;
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080/"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });

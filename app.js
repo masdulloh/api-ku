b9af9f784b49457ff90fa24bfa035c1f0fbafcd2
masdulloh <ma089983@gmail.com>
2020-01-06 23:48:14 +0700

    newww

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const app = express();
 const rajaRouter = require("./api/rajaongkir/rajaongkir.router");
 const port = process.env.PORT || 4000;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 

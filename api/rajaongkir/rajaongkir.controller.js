15150cbc7a514cd8ffef89b468842446369caf77
masdulloh <ma089983@gmail.com>
2020-01-21 23:11:56 +0700

    hhhhhh

--- api/rajaongkir/rajaongkir.controller.js
+++ api/rajaongkir/rajaongkir.controller.js
@@ -28,41 +28,92 @@ module.exports = {
 
     pushMoota: (req, res) => {
         let myMootaPush={
-            amount: req.body.amount,
-            description: req.body.description
+            amount: req.body[0].amount,
+            description: req.body[0].description
         };
+        console.log(myMootaPush);
         let allData= [];
             db.collection('orders').where('verif', '==', myMootaPush.description).get()
             .then(snapshot => {
                 if (snapshot.empty) {
+                    res.set({
+                        'Access-Control-Allow-Origin': ['*'],
+                        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+                        'Access-Control-Allow-Headers': 'Content-Type',
+                        'Content-Type': 'application/json',
+                        'Content-Length': '123',
+                        'ETag': '12345'
+                    }).status(200).send(allData)
+                    
                     //console.log('No city matching documents.');
-                    res.send(allData)
+                    //res.send(allData)
                     return;
                 } 
                 snapshot.forEach(doc => {
+                    console.log(doc.data())
+                    allData.push(doc.data());
                     if (Number(myMootaPush.amount)===doc.data().gross && myMootaPush.description===doc.data().verif){
-                        allData.push(doc.data());
+                        
                         db.collection('orders').doc(doc.id)
                         .update({ paystatus: 'Paid' })
                         .then(() => {
                             //console.log('Update pembayaran berhasil');
-                            res.send(allData);
+                            //res.send(allData);
+                            res.set({
+                                'Access-Control-Allow-Origin': ['*'],
+                                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+                                'Access-Control-Allow-Headers': 'Content-Type',
+                                'Content-Type': 'application/json',
+                                'Content-Length': '123',
+                                'ETag': '12345'
+                            }).status(200).send(allData)
+                            
                         }).catch(err => {
-                            res.send(allData);
+                            //res.send(allData);
                             //console.log('Error update payment status: ', err);
+                            res.set({
+                                'Access-Control-Allow-Origin': ['*'],
+                                'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+                                'Access-Control-Allow-Headers': 'Content-Type',
+                                'Content-Type': 'application/json',
+                                'Content-Length': '123',
+                                'ETag': '12345'
+                            }).status(200).send(allData)
+                            
                         })
                         return;
                     } else {
+                        res.set({
+                            'Access-Control-Allow-Origin': ['*'],
+                            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+                            'Access-Control-Allow-Headers': 'Content-Type',
+                            'Content-Type': 'application/json',
+                            'Content-Length': '123',
+                            'ETag': '12345'
+                        }).status(200).send(allData)
+                        
                         //console.log('ELSE GAGAL');
-                        res.send(allData);
+                        //res.send(allData);
                         return;
                     }
+                    
+                    
                 });
             }).catch(err => {
                 //console.log('Error getting documents', err);
-                res.send(allData)
+                //res.send(allData)
+                res.set({
+                    'Access-Control-Allow-Origin': ['*'],
+                    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Content-Type': 'application/json',
+                    'Content-Length': '123',
+                    'ETag': '12345'
+                }).status(200).send(allData)
+                
                 return;
             })
+            
     },
 
 
